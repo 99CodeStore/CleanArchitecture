44c48e62c36bbbcafad1b60b6793b4c99f18a555
Bheemsen Singh <bheem_stn@hotmail.com>
2021-12-31 15:16:18 +0530

    Api and Infrastructure.Data project are added.

---
+++ CleanArchitecture.Infrastructure.Data/Context/UniversityDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CleanArchitecture.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data.Context
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+        public DbSet<Course> courses { get; set; }
+
+    }
+}
