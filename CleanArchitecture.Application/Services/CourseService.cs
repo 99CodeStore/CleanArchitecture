0c2f30524524a6f600e2bf675dda4babfe5d30d7
Bheemsen Singh <bheem_stn@hotmail.com>
2021-12-31 16:08:07 +0530

    Implementation of Service and Repository GetCourse Method.

---
+++ CleanArchitecture.Application/Services/CourseService.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.Models;
+using CleanArchitecture.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository repository;
+
+        public CourseService(ICourseRepository repository)
+        {
+            this.repository = repository;
+        }
+        public CourseModel GetCourses()
+        {
+            return new CourseModel { Courses = repository.GetCourses() };
+        }
+    }
+}
---
+++ CleanArchitecture.Infrastructure.Data/Repository/CourseRepository.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Domain.Entities;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Data.Repository
+{
+    public class CourseRepository: ICourseRepository
+    {
+        private readonly UniversityDbContext context;
+
+        public CourseRepository(UniversityDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Course> GetCourses()
+        {
+            return context.courses;
+        }
+    }
+}
